d1ecc1afa3f5c5ef97dcfd37a58359d6a56bfdd2
handson <musahandson568@gmail.com>
2025-01-29 13:25:48 +0300

    commit

--- My First Assgm.py
+++ My First Assgm.py
@@ -4,6 +4,12 @@ age=26
 float(1.8)
 print("Age:26")
 print("Height:1.8M")
+#list
+cars=["Mercedes","Honda","Toyota","Audi","Volkswagen"]
+print(cars)
+length=[len(car) for car in cars]
+length={car:len(car) for car in cars}
+print(length)
 
 
 
